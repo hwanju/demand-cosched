64948aa98240e8b399e7c97caebf02d13696652e
Hwanju Kim <hwandori@gmail.com>
2012-06-10 15:17:15 +0900

    kvm pvspinlock is fixed (debugfs kvm dir is changed to kvm_guest)

--- arch/x86/kernel/kvm.c
+++ arch/x86/kernel/kvm.c
@@ -743,9 +743,9 @@ static struct dentry *d_kvm_debug;
 
 struct dentry *kvm_init_debugfs(void)
 {
-	d_kvm_debug = debugfs_create_dir("kvm", NULL);
+	d_kvm_debug = debugfs_create_dir("kvm_guest", NULL);
 	if (!d_kvm_debug)
-		printk(KERN_WARNING "Could not create 'kvm' debugfs directory\n");
+		printk(KERN_WARNING "Could not create 'kvm_guest' debugfs directory\n");
 
 	return d_kvm_debug;
 }
